ac437b527869ecc924a2e55e906f60348e546644
Thiago <56048613+DocTheodore@users.noreply.github.com>
2023-11-23 09:38:22 -0300

    Atualização do arquivo SQL
    - adicionado novas variáveis nas tabelas
    - adicionando auto-increment no ID

--- Diesel/database/database_diesel.sql
+++ Diesel/database/database_diesel.sql
@@ -5,22 +5,22 @@ USE diesel;
 -- Geraçao das Tabelas --------------------------------------
 -- Tabela Cliente
 CREATE TABLE IF NOT EXISTS usuario (
-    id INT PRIMARY KEY,
-    nome VARCHAR(255),
-    senha VARCHAR(255),
-    idade INT,
-    saldo DECIMAL(10, 2),
-    nacionalidade VARCHAR(255),
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    senha VARCHAR(255) NOT NULL,
+    idade INT NOT NULL,
+    saldo DECIMAL(10, 2) NOT NULL,
+    nacionalidade VARCHAR(255) NOT NULL,
     lista_amigos TEXT
 );
 
 -- Tabela Jogos
 CREATE TABLE IF NOT EXISTS jogos (
-    id INT PRIMARY KEY,
-    nome VARCHAR(255),
-    desenvolvedor VARCHAR(255),
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    desenvolvedor VARCHAR(255) NOT NULL,
     publisher VARCHAR(255),
-    url_imagem VARCHAR(255),
+    url_imagem VARCHAR(255) NOT NULL,
     nota_geral DECIMAL(3, 2),
     preco DECIMAL(10, 2),
     quantidade_vendas INT,
@@ -29,17 +29,17 @@ CREATE TABLE IF NOT EXISTS jogos (
 
 -- Tabela Desenvolvedor
 CREATE TABLE IF NOT EXISTS desenvolvedor (
-    cnpj VARCHAR(20) PRIMARY KEY,
-    nome VARCHAR(255),
-    senha VARCHAR(255),
+    cnpj VARCHAR(20) PRIMARY KEY NOT NULL,
+    nome VARCHAR(255) NOT NULL,
+    senha VARCHAR(255) NOT NULL,
     jogos_publicados TEXT
 );
 
 -- Tabela Eventos
 CREATE TABLE IF NOT EXISTS eventos (
-    id INT PRIMARY KEY,
-    nome VARCHAR(255),
-    duracao_dias INT,
+    id INT PRIMARY KEY NOT NULL,
+    nome VARCHAR(255) NOT NULL,
+    duracao_dias INT NOT NULL,
     descricao TEXT,
     porcentagem_promocao DECIMAL(5, 2)
 );
